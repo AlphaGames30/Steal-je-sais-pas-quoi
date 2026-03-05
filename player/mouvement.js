9317d8aa6823dc311eb87283597e24d51a3324a9
AlphaGames30 <edonis.imbert@gmail.com>
2026-03-05 12:14:26 +0100

    Update mouvement.js

--- player/mouvement.js
+++ player/mouvement.js
@@ -1,12 +1,15 @@
-export function movePlayer(player, keys){
+export function movePlayer(player, keys, moveVector){
 
-    const speed = player.speed
+    // PC (clavier)
+    if(keys["z"] || keys["w"]) player.z -= player.speed
+    if(keys["s"]) player.z += player.speed
+    if(keys["q"] || keys["a"]) player.x -= player.speed
+    if(keys["d"]) player.x += player.speed
 
-    if(keys["z"] || keys["w"]) player.z -= speed
-    if(keys["s"]) player.z += speed
-    if(keys["q"] || keys["a"]) player.x -= speed
-    if(keys["d"]) player.x += speed
+    // Mobile (glisser sur l'écran)
+    player.x += moveVector.x * player.speed
+    player.z += moveVector.z * player.speed
 
+    // Mettre à jour la position du mesh
     player.mesh.position.set(player.x, player.y, player.z)
-
 }
